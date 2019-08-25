46531e4b3b3db9b1265d930198df62151be47647
Alex Oskotsky <aoskotsky@amplify.com>
2019-08-25 19:13:03 -0400

    handle comments better

--- test/helpers/terraform-config/backend.tf
+++ test/helpers/terraform-config/backend.tf
@@ -1,12 +1,17 @@
+// a comment
 provider "aws" {
   region = var.region
 }
 
+# another comment
 provider "aws" {
   region = (var.backup_region)
   alias  = "backup"
 }
 
+/*
+one last comment
+*/
 terraform { required_version = "0.12"}
 
 terraform {
