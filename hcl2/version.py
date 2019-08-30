3b69c3dcd03fc640c864791436a207de38709c08
Alex Oskotsky <aoskotsky@amplify.com>
2019-08-30 12:49:13 -0400

    support for expressions

--- hcl2/version.py
+++ hcl2/version.py
@@ -1,4 +1,4 @@
 """Place of record for the package version"""
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __git_hash__ = "GIT_HASH"
--- test/helpers/terraform-config/iam.tf
+++ test/helpers/terraform-config/iam.tf
@@ -18,3 +18,20 @@ data "aws_iam_policy_document" "policy" {
   }
 }
 
+data "aws_iam_policy_document" "s3_proxy_policy" {
+  statement {
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+    for
+    bucket_name
+    in
+    local.buckets_to_proxy :
+    "arn:aws:s3:::${bucket_name}/*"
+    if
+    substr(bucket_name, 0, 1) == "l"
+    ]
+  }
+}
