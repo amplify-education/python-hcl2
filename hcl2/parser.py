726bf0c7e1ad9c6a16b25b5bc34188cf608ca637
Erez Sh <erezshin@gmail.com>
2021-01-12 17:37:13 -0500

    Restore cache (accidentally left disabled)

--- hcl2/parser.py
+++ hcl2/parser.py
@@ -11,9 +11,9 @@ from hcl2.transformer import DictTransformer
 PARSER_FILE = os.path.join(dirname(__file__), '.lark_cache.bin')
 
 hcl2 = Lark.open(
-  'hcl2.lark',
-  parser='lalr',
-  # cache=PARSER_FILE,  # Disable/Delete file to effect changes to the grammar
-  rel_to=__file__ ,
-  transformer=DictTransformer()
+    'hcl2.lark',
+    parser='lalr',
+    cache=PARSER_FILE,  # Disable/Delete file to effect changes to the grammar
+    rel_to=__file__,
+    transformer=DictTransformer()
 )
