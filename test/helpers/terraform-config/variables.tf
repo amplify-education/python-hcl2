4c1c47aed92359779b921ac4614f4c59ab197ba3
Ryan King <ryan.king@color.com>
2022-01-24 15:46:01 -0800

    fix issue of whitespace around for expressions
    I noticed that the parser would error if whitespace was included after
    the => in a for expression like the one I added to the tests.

--- test/helpers/terraform-config/variables.tf
+++ test/helpers/terraform-config/variables.tf
@@ -46,4 +46,9 @@ locals {
     length(var.forwarding_rules_template.replace_with_target_ips) > 0 &&
     length(var.forwarding_rules_template.exclude_cidrs) > 0
   )
+
+  for_whitespace = { for i in [1, 2, 3] :
+    i =>
+    i
+  }
 }
