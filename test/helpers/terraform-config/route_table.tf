97594a1ac705b00cbbb051fc8f4e8c319fb3e82d
saksham <Saksham.Bhayana@kyndryl.com>
2023-03-29 16:32:20 +0530

    Handled ternary multi-line expression

--- test/helpers/terraform-config/route_table.tf
+++ test/helpers/terraform-config/route_table.tf
@@ -11,7 +11,8 @@ resource "aws_route" "tgw" {
 }
 
 resource "aws_route" "tgw-dot-index" {
-  count                  = var.tgw_name == "" ? 0 : var.number_of_az
+  count                  = (var.tgw_name == ""
+                        ? 0 : var.number_of_az)
   route_table_id         = aws_route_table.rt[count.index].id
   destination_cidr_block = "10.0.0.0/8"
   transit_gateway_id     = data.aws_ec2_transit_gateway.tgw.0.id
