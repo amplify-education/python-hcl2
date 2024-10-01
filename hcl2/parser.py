51c0972c36efd67094f7ac0ca272286a33b737ac
Sam Weaver <sweaver@relaypro.com>
2024-10-01 14:53:11 -0400

    Simplify reconstruction to just output the spaces as is around newlines

--- hcl2/parser.py
+++ hcl2/parser.py
@@ -3,8 +3,7 @@ from pathlib import Path
 
 from lark import Lark, Token
 from lark.reconstruct import Reconstructor
-
-from .indenter import TerraformIndenter
+from lark.utils import is_id_continue
 
 PARSER_FILE = Path(__file__).absolute().resolve().parent / ".lark_cache.bin"
 
@@ -16,65 +15,65 @@ hcl2 = Lark.open(
     rel_to=__file__,
     propagate_positions=True,
     maybe_placeholders=False,  # Needed for reconstruction
-    postlex=TerraformIndenter(),
 )
 
 SPACE_AFTER = set(',+-*/~@<>="|:')
 SPACE_BEFORE = (SPACE_AFTER - set(",:")) | set("'")
-
-
-def _special_symbol(sym):
-    return Token("SPECIAL", sym.name)
-
+DIGITS = set("0123456789")
+IDENT_JOINERS = set(".")
 
 def _postprocess_reconstruct(items):
-    stack = ["\n"]
-    actions = []
-    last_was_whitespace = True
-    for item in items:
-        if isinstance(item, Token) and item.type == "SPECIAL":
-            actions.append(item.value)
-        else:
-            if actions:
-                assert actions[0] == "_NL", actions
+    """
+    Postprocess the stream of tokens derived from the AST during reconstruction.
 
-                for a in actions[1:]:
-                    if a == "_NL":
-                        yield "\n"
-                    elif a == "_INDENT":
-                        stack.append(stack[-1] + " " * 2)
-                    elif a == "_DEDENT":
-                        stack.pop()
-                    else:
-                        assert False, a
-                actions.clear()
-                yield stack[-1]
-                last_was_whitespace = True
-            if not last_was_whitespace:
-                if item[0] in SPACE_BEFORE:
-                    yield " "
-            yield item
-            last_was_whitespace = item[-1].isspace()
-            if not last_was_whitespace:
-                if item[-1] in SPACE_AFTER:
-                    yield " "
-                    last_was_whitespace = True
+    For HCL2, this is used exclusively for adding whitespace in the right locations.
+    """
+    prev_item = ""
+    for item in items:
+        # see if we need to add space after the previous identifier
+        if (
+            prev_item
+            and item
+            and prev_item[-1] not in SPACE_AFTER
+            and not prev_item[-1].isspace()
+            and is_id_continue(prev_item[-1])
+            and not item[0] in IDENT_JOINERS
+            and not prev_item[-1] in DIGITS
+        ):
+            yield " "
+            prev_item = " "
+
+        # if the next character expects us to add a space before it
+        if (
+            prev_item
+            and not prev_item[-1].isspace()
+            and prev_item[-1] not in SPACE_AFTER
+        ):
+            if item[0] in SPACE_BEFORE:
+                yield " "
+                prev_item = " "
+
+        # print the actual token
+        yield item
+
+        if item and item[-1] in SPACE_AFTER:
+            yield " "
+
+        # store the previous item as we continue
+        prev_item = item
     yield "\n"
 
 
 class HCLReconstructor:
     def __init__(self, parser):
-        self._recons = Reconstructor(
-            parser,
-            {
-                "_NL": _special_symbol,
-                "_DEDENT": _special_symbol,
-                "_INDENT": _special_symbol,
-            },
-        )
+        self._recons = Reconstructor(parser)
 
     def reconstruct(self, tree):
-        return self._recons.reconstruct(tree, _postprocess_reconstruct)
+        return self._recons.reconstruct(
+            tree,
+            _postprocess_reconstruct,
+            insert_spaces=False,
+        )
 
 
 hcl2_reconstructor = HCLReconstructor(hcl2)
