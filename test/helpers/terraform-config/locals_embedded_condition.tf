c543c266a615000d8630b57952e9383bcedd0d4b
Kamil Kozik <kkozik@amplify.com>
2025-03-19 13:58:14 +0100

    fix reconstruction of `null` this fixes #195

--- test/helpers/terraform-config/locals_embedded_condition.tf
+++ test/helpers/terraform-config/locals_embedded_condition.tf
@@ -2,5 +2,6 @@ locals {
   terraform = {
     channels = (local.running_in_ci ? local.ci_channels : local.local_channels),
     authentication = [],
+    foo = null,
   }
 }
--- test/unit/test_builder.py
+++ test/unit/test_builder.py
@@ -44,6 +44,7 @@ class TestBuilder(TestCase):
             terraform={
                 "channels": "${local.running_in_ci ? local.ci_channels : local.local_channels}",
                 "authentication": [],
+                "foo": None,
             },
         )
 
