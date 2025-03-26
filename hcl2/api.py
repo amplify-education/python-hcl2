8edc8a79f37a41bc258f3d4c8427b451fb0bf6e9
Kamil Kozik <115631738+kkozik-amplify@users.noreply.github.com>
2025-03-26 10:20:58 +0100

    some refactoring of reconstructor.py and api.py (#202)

--- hcl2/api.py
+++ hcl2/api.py
@@ -1,9 +1,10 @@
 """The API that will be exposed to users of this package"""
 from typing import TextIO
 
-from lark.tree import Tree as AST
-from hcl2.parser import parser
+from lark.tree import Tree
+from hcl2.parser import parser, reconstruction_parser
 from hcl2.transformer import DictTransformer
+from hcl2.reconstructor import HCLReconstructor, HCLReverseTransformer
 
 
 def load(file: TextIO, with_meta=False) -> dict:
@@ -22,61 +23,45 @@ def loads(text: str, with_meta=False) -> dict:
     parameters to the output dict. Default to false.
     """
     # append new line as a workaround for https://github.com/lark-parser/lark/issues/237
-    # Lark doesn't support a EOF token so our grammar can't look for "new line or end of file"
+    # Lark doesn't support EOF token so our grammar can't look for "new line or end of file"
     # This means that all blocks must end in a new line even if the file ends
     # Append a new line as a temporary fix
     tree = parser().parse(text + "\n")
     return DictTransformer(with_meta=with_meta).transform(tree)
 
 
-def parse(file: TextIO) -> AST:
+def parse(file: TextIO) -> Tree:
     """Load HCL2 syntax tree from a file.
     :param file: File with hcl2 to be loaded as a dict.
     """
     return parses(file.read())
 
 
-def parses(text: str) -> AST:
+def parses(text: str) -> Tree:
     """Load HCL2 syntax tree from a string.
     :param text: Text with hcl2 to be loaded as a dict.
     """
-    # defer this import until this method is called, due to the performance hit
-    # of rebuilding the grammar without cache
-    from hcl2.parser import (  # pylint: disable=import-outside-toplevel
-        reconstruction_parser,
-    )
-
     return reconstruction_parser().parse(text)
 
 
-def transform(ast: AST, with_meta=False) -> dict:
+def transform(ast: Tree, with_meta=False) -> dict:
     """Convert an HCL2 AST to a dictionary.
     :param ast: HCL2 syntax tree, output from `parse` or `parses`
+    :param with_meta: If set to true then adds `__start_line__` and `__end_line__`
+        parameters to the output dict. Default to false.
     """
     return DictTransformer(with_meta=with_meta).transform(ast)
 
 
-def reverse_transform(hcl2_dict: dict) -> AST:
+def reverse_transform(hcl2_dict: dict) -> Tree:
     """Convert a dictionary to an HCL2 AST.
-    :param dict: a dictionary produced by `load` or `transform`
+    :param hcl2_dict: a dictionary produced by `load` or `transform`
     """
-    # defer this import until this method is called, due to the performance hit
-    # of rebuilding the grammar without cache
-    from hcl2.reconstructor import (  # pylint: disable=import-outside-toplevel
-        hcl2_reverse_transformer,
-    )
-
-    return hcl2_reverse_transformer.transform(hcl2_dict)
+    return HCLReverseTransformer().transform(hcl2_dict)
 
 
-def writes(ast: AST) -> str:
+def writes(ast: Tree) -> str:
     """Convert an HCL2 syntax tree to a string.
     :param ast: HCL2 syntax tree, output from `parse` or `parses`
     """
-    # defer this import until this method is called, due to the performance hit
-    # of rebuilding the grammar without cache
-    from hcl2.reconstructor import (  # pylint: disable=import-outside-toplevel
-        hcl2_reconstructor,
-    )
-
-    return hcl2_reconstructor.reconstruct(ast)
+    return HCLReconstructor(reconstruction_parser()).reconstruct(ast)
