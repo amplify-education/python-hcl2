b9018119f8bd2e923ef460364f08f2d8d60e2bc1
Kamil Kozik <kkozik@amplify.com>
2025-04-07 12:49:01 +0200

    `hcl2.builder.Builder` - nested blocks support

--- test/helpers/terraform-config/blocks.tf
+++ test/helpers/terraform-config/blocks.tf
@@ -4,4 +4,19 @@ block {
 
 block "label" {
   b = 2
+  nested_block_1 "a" {
+    foo = "bar"
+  }
+
+  nested_block_1 "a" "b" {
+    bar = "foo"
+  }
+
+  nested_block_1 {
+    foobar = "barfoo"
+  }
+
+  nested_block_2 {
+    barfoo = "foobar"
+  }
 }
