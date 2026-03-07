deaf0939b199aec1090ad9d89ac5dc87dade9b2c
Kamil Kozik <kkozik@amplify.com>
2026-03-07 23:12:48 +0100

    remove unused test files

--- test/helpers/terraform-config/resource_keyword_attribute.tf
+++
@@ -1,8 +0,0 @@
-resource "custom_provider_resource" "resource_name" {
-  name           = "resource_name"
-  attribute      = "attribute_value"
-  if             = "attribute_value2"
-  in             = "attribute_value3"
-  for            = "attribute_value4"
-  for_each       = "attribute_value5"
-}
